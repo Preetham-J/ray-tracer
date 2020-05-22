c5c33d92c9f6addc1490f1968c895193645304be
Preetham Jayaram <35173316+Preetham-J@users.noreply.github.com>
2020-05-22 15:18:40 -0400

    Add the Sphere class and basic ray tracing ability.

--- main.cpp
+++ main.cpp
@@ -10,18 +10,78 @@
 #include <vector>
 #include "geometry.hpp"
 
-void render()
+
+class Sphere
+{
+    public:
+        Sphere(const Vec3f &center, const float &radius) :
+            center_(center), radius_(radius)
+        {}
+
+        // Determine whether a ray intersects the sphere
+        // Based on the geometric solution to the ray-sphere intersection algorithm 
+        bool RayIntersect(const Vec3f &origin, const Vec3f &direction, float &t0) const
+        {
+            float t1, tca, d, thc;
+            Vec3f L = center_ - origin;
+            tca = L * direction;
+            d = (L * L) - (tca * tca);
+            if (d > radius_ * radius_)
+            {
+                return false;
+            }
+            thc = sqrtf(radius_ * radius_ - d);
+            t0 = tca - thc;
+            t1 = tca + thc;
+            // If t0 is negative, use t1 instead
+            if (t0 > t1)
+            {
+                t0 = t1;
+            }
+            // If both are negative, no intersection
+            if (t0 < 0)
+            {
+                return false;
+            }
+            // Otherwise, there is an intersection
+            return true;
+        }
+
+    private:
+        Vec3f center_;
+        float radius_;
+};
+
+// Cast a ray from the origin to the sphere in the given direction
+Vec3f CastRay(const Vec3f& origin, const Vec3f& direction, const Sphere& sphere)
+{
+    float sphere_distance = std::numeric_limits<float>::max();
+    // If the ray doesn't intersect, return background colour
+    if (!sphere.RayIntersect(origin, direction, sphere_distance))
+    {
+        return Vec3f(0.2, 0.7, 0.8);
+    }
+    // Else return set colour
+    return Vec3f(0.4, 0.4, 0.3);
+}
+
+// Render image
+void render(const Sphere &sphere)
 {
     const int width = 1024;
     const int height = 768;
+    const float fov = M_PI/3.0;
     std::vector<Vec3f> frame_buffer(width * height);
 
-    // Initialize frame buffer with random RGB values
+    // Fill frame buffer with ray tracing results
     for (std::size_t j = 0; j < height; j++)
     {
         for (std::size_t i = 0; i < width; i++)
         {
-            frame_buffer[i + j*width] = Vec3f(j / float(height), i / float(width), 0);
+            float x = (2*(i + 0.5)/(float)width - 1) * tan(fov/2.0)*width/(float)height;
+            float y = -(2*(j + 0.5)/(float)height- 1) * tan(fov/2.0);
+            Vec3f direction = Vec3f(x, y, -1).normalise();
+            frame_buffer[i + j*width] = CastRay(Vec3f(0, 0, 0), direction, sphere);
         }
     }
 
@@ -41,6 +101,7 @@ void render()
 
 int main()
 {
-    render();
+    Sphere sphere(Vec3f(-3, 0, -16), 2);
+    render(sphere);
     return 0;
 }
